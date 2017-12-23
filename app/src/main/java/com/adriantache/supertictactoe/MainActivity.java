a57a27c254ead5a72c6ae50e209a1271d559f58b
Adrian Tache <contact@adriantache.com>
2017-12-23 23:59:53 +0200

    Set up initial layout

--- app/src/main/java/com/adriantache/supertictactoe/MainActivity.java
+++ app/src/main/java/com/adriantache/supertictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.adriantache.supertictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void reset(View view){
+        //todo implement reset functionality
+        return;
     }
 }
